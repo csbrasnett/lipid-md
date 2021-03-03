b13fe401c86f6bb446b1345e6c12ee3e18355104
Chris Brasnett <christopher.brasnett@bristol.ac.uk>
2021-03-03 16:41:55 +0000

    add path to headers

--- QIIDsurface_generator.py
+++ QIIDsurface_generator.py
@@ -9,7 +9,6 @@ author: Chris Brasnett, University of Bristol, christopher.brasnett@bristol.ac.u
 import numpy as np
 from skimage import measure
 from mdma import atom
-import matplotlib.pyplot as plt
 from QIIDcurvature import main as curvature
 
 def trig_choose(a,var):
@@ -88,7 +87,81 @@ def permO7(h,k,l,x,y,z,lamb):
 def permO8(h,k,l,x,y,z,lamb):
     return O_pqr('s','c','c',h,k,l,x,y,z,lamb)
 
-def generator(a_1,a_2,a_3,l,curvature_calculations = False):
+
+    
+def D_nodal_approx(x,y,z,lamb):
+
+    term1=permE1(1,1,1,x,y,z,lamb)+permE2(1,1,1,x,y,z,lamb)+permE3(1,1,1,x,y,z,lamb)+permE4(1,1,1,x,y,z,lamb)
+       
+    term2=0.1407*(permE1(1,1,1,x,y,z,lamb)+permE2(1,1,1,x,y,z,lamb)+
+                  permE3(1,1,1,x,y,z,lamb)+permE4(1,1,1,x,y,z,lamb)+
+                  permO1(1,1,1,x,y,z,lamb)+permO2(1,1,1,x,y,z,lamb)+
+                  permO3(1,1,1,x,y,z,lamb)+permO4(1,1,1,x,y,z,lamb))
+
+    term3=0.0200*(permE1(3,1,1,x,y,z,lamb)+permE2(3,1,1,x,y,z,lamb)-
+                  permE3(3,1,1,x,y,z,lamb)+permE4(3,1,1,x,y,z,lamb)+
+                  permO1(3,1,1,x,y,z,lamb)+permO2(3,1,1,x,y,z,lamb)-
+                  permO3(3,1,1,x,y,z,lamb)-permO4(3,1,1,x,y,z,lamb))
+    
+    term4=0.0138*(permE1(3,1,3,x,y,z,lamb)-permE2(3,1,3,x,y,z,lamb)+
+                  permE3(3,1,3,x,y,z,lamb)-permE4(3,1,3,x,y,z,lamb)+
+                  permO1(3,1,3,x,y,z,lamb)-permO2(3,1,3,x,y,z,lamb)+
+                  permO3(3,1,3,x,y,z,lamb)+permO4(3,1,3,x,y,z,lamb))
+    
+    term5=0.0028*(permE1(1,1,5,x,y,z,lamb)+permE2(1,1,5,x,y,z,lamb)+
+                  permE3(1,1,5,x,y,z,lamb)+permE4(1,1,5,x,y,z,lamb)+
+                  permO1(1,1,5,x,y,z,lamb)+permO2(1,1,5,x,y,z,lamb)+
+                  permO3(1,1,5,x,y,z,lamb)+permO4(1,1,5,x,y,z,lamb))
+
+    term6=0.0021*(permE1(3,3,3,x,y,z,lamb)+permE2(3,3,3,x,y,z,lamb)+
+                  permE3(3,3,3,x,y,z,lamb)+permE4(3,3,3,x,y,z,lamb)+
+                  permO1(3,3,3,x,y,z,lamb)+permO2(3,3,3,x,y,z,lamb)+
+                  permO3(3,3,3,x,y,z,lamb)+permO4(3,3,3,x,y,z,lamb))
+    
+    term7=0.0001*(permE1(3,1,5,x,y,z,lamb)+permE2(3,1,5,x,y,z,lamb)-
+                  permE3(3,1,5,x,y,z,lamb)-permE4(3,1,5,x,y,z,lamb)+
+                  permO1(3,1,5,x,y,z,lamb)+permO2(3,1,5,x,y,z,lamb)-
+                  permO3(3,1,5,x,y,z,lamb)-permO4(3,1,5,x,y,z,lamb))
+    
+    return term1+term2+term3-term4-term5-term6+term7
+
+    
+
+def P_nodal_approx(x,y,z,lamb):
+    
+    term1=permE1(1,0,0,x,y,z,lamb)
+    
+    #term2=0.2260*(permE1(1,0,0,x,y,z,lamb)+permO1(1,0,0,x,y,z,lamb))
+    
+    #term3=0.0516*(permE1(1,1,1,x,y,z,lamb)+permO1(1,1,1,x,y,z,lamb))
+    
+    #term4=0.0196*(permE1(2,1,0,x,y,z,lamb)+permO1(2,1,0,x,y,z,lamb))
+    
+    #term5=0.0027*(permE1(3,0,0,x,y,z,lamb)+permO1(3,0,0,x,y,z,lamb))
+    
+    return term1#+term2-term3-term4-term5
+
+
+def G_nodal_approx(x,y,z,lamb):
+    
+    term1=permE5(1,1,0,x,y,z,lamb)+permO5(1,1,0,x,y,z,lamb)
+    
+    # term2=0.3435*(permE4(1,1,0,x,y,z,lamb)+permO5(1,1,0,x,y,z,lamb))
+    
+    # term3=0.0202*(permE6(0,3,1,x,y,z,lamb)+permO6(0,3,1,x,y,z,lamb))
+    
+    # term4=0.0106*(permE7(2,2,2,x,y,z,lamb)+permO7(2,2,2,x,y,z,lamb))
+    
+    # term5=0.0298*(permE6(2,1,3,x,y,z,lamb)+permO6(2,1,3,x,y,z,lamb))
+    
+    # term6=0.0016*(permE8(3,0,3,x,y,z,lamb)+permO8(3,0,3,x,y,z,lamb))
+    
+    # term7=0.0011*(permE4(1,1,4,x,y,z,lamb)+permO5(1,1,4,x,y,z,lamb))
+    
+    return term1#+term2-term3-term4+term5+term6-term7
+    
+
+def generator(a_1,a_2,a_3,l,D = False, P = False, G = False):
 
     lamb = (1/l)/2
 
@@ -96,40 +169,13 @@ def generator(a_1,a_2,a_3,l,curvature_calculations = False):
     X,Y,Z = np.mgrid[a_1-l:a_1+l:(2*l * 1j), 
                      a_2-l:a_2+l:(2*l * 1j), 
                      a_3-l:a_3+l:(2*l * 1j)]
-
-    term1=permE1(1,1,1,X,Y,Z,lamb)+permE2(1,1,1,X,Y,Z,lamb)+permE3(1,1,1,X,Y,Z,lamb)+permE4(1,1,1,X,Y,Z,lamb)
-       
-    term2=0.1407*(permE1(1,1,1,X,Y,Z,lamb)+permE2(1,1,1,X,Y,Z,lamb)+
-                  permE3(1,1,1,X,Y,Z,lamb)+permE4(1,1,1,X,Y,Z,lamb)+
-                  permO1(1,1,1,X,Y,Z,lamb)+permO2(1,1,1,X,Y,Z,lamb)+
-                  permO3(1,1,1,X,Y,Z,lamb)+permO4(1,1,1,X,Y,Z,lamb))
     
-    term3=0.0200*(permE1(3,1,1,X,Y,Z,lamb)+permE2(3,1,1,X,Y,Z,lamb)-
-                  permE3(3,1,1,X,Y,Z,lamb)+permE4(3,1,1,X,Y,Z,lamb)+
-                  permO1(3,1,1,X,Y,Z,lamb)+permO2(3,1,1,X,Y,Z,lamb)-
-                  permO3(3,1,1,X,Y,Z,lamb)-permO4(3,1,1,X,Y,Z,lamb))
-    
-    term4=0.0138*(permE1(3,1,3,X,Y,Z,lamb)-permE2(3,1,3,X,Y,Z,lamb)+
-                  permE3(3,1,3,X,Y,Z,lamb)-permE4(3,1,3,X,Y,Z,lamb)+
-                  permO1(3,1,3,X,Y,Z,lamb)-permO2(3,1,3,X,Y,Z,lamb)+
-                  permO3(3,1,3,X,Y,Z,lamb)+permO4(3,1,3,X,Y,Z,lamb))
-    
-    term5=0.0028*(permE1(1,1,5,X,Y,Z,lamb)+permE2(1,1,5,X,Y,Z,lamb)+
-                  permE3(1,1,5,X,Y,Z,lamb)+permE4(1,1,5,X,Y,Z,lamb)+
-                  permO1(1,1,5,X,Y,Z,lamb)+permO2(1,1,5,X,Y,Z,lamb)+
-                  permO3(1,1,5,X,Y,Z,lamb)+permO4(1,1,5,X,Y,Z,lamb))
-    
-    term6=0.0021*(permE1(3,3,3,X,Y,Z,lamb)+permE2(3,3,3,X,Y,Z,lamb)+
-                  permE3(3,3,3,X,Y,Z,lamb)+permE4(3,3,3,X,Y,Z,lamb)+
-                  permO1(3,3,3,X,Y,Z,lamb)+permO2(3,3,3,X,Y,Z,lamb)+
-                  permO3(3,3,3,X,Y,Z,lamb)+permO4(3,3,3,X,Y,Z,lamb))
-    
-    term7=0.0001*(permE1(3,1,5,X,Y,Z,lamb)+permE2(3,1,5,X,Y,Z,lamb)-
-                  permE3(3,1,5,X,Y,Z,lamb)-permE4(3,1,5,X,Y,Z,lamb)+
-                  permO1(3,1,5,X,Y,Z,lamb)+permO2(3,1,5,X,Y,Z,lamb)-
-                  permO3(3,1,5,X,Y,Z,lamb)-permO4(3,1,5,X,Y,Z,lamb))
-    
-    surf_eq =  term1+term2+term3-term4-term5-term6+term7
+    if D:
+        surf_eq = D_nodal_approx(X,Y,Z, lamb)
+    elif P:
+        surf_eq = P_nodal_approx(X,Y,Z, lamb)
+    elif G:
+        surf_eq = G_nodal_approx(X,Y,Z, lamb)
     
     #find vertices on the surface
     vertices, simplices,normals, values = measure.marching_cubes_lewiner(surf_eq, level=0)
@@ -145,8 +191,8 @@ def generator(a_1,a_2,a_3,l,curvature_calculations = False):
     correct_positions[0:,1] = table[0:,1]+(a_2-l)
     correct_positions[0:,2] = table[0:,2]+(a_3-l)
     
-    curvatures = np.zeros(0)
-    if curvature_calculations==True:
+    if D==True:
+        curvatures = np.zeros(0)
         for i in range(len(correct_positions)):
             #for an update on progress
             #don't know how to actually make this regular, but it doesn't really matter
@@ -160,13 +206,15 @@ def generator(a_1,a_2,a_3,l,curvature_calculations = False):
         
     else:
         return correct_positions
-    
+
 
 
 if __name__=='__main__':
-    points, curvatures = generator(40,40,40,20, curvature_calculations = True)
+
     
     
+    points, curvatures = generator(40,40,40,40, D = True)    
+        
     curvature_bins = np.linspace(curvatures.min(), 0, 30)
     mids_curvature_bins = (curvature_bins[:-1]+curvature_bins[1:])/2
     
@@ -180,14 +228,25 @@ if __name__=='__main__':
         opstr+= [str(i)]*len(find)
     
     points_out = points[point_inds]
-            
-    with open('temp1.atom', 'w') as f:
+                
+    with open('../D.atom', 'w') as f:
         atom.write(points_out, np.array([[points_out[0:,0].min(), points_out[0:,0].max()],
-                                         [points_out[0:,1].min(), points_out[0:,1].max()],
-                                         [points_out[0:,2].min(), points_out[0:,2].max()]]), f, opstr)
-    
-    
-    
+                                          [points_out[0:,1].min(), points_out[0:,1].max()],
+                                          [points_out[0:,2].min(), points_out[0:,2].max()]]), f, opstr)
+     
+
+
+    # points = generator(25,25,25,50, P = True)    
+    # points_out = points
+        
+                
+    # with open('../temp1.atom', 'w') as f:
+    #     atom.write(points_out, np.array([[points_out[0:,0].min(), points_out[0:,0].max()],
+    #                                      [points_out[0:,1].min(), points_out[0:,1].max()],
+    #                                      [points_out[0:,2].min(), points_out[0:,2].max()]]), f)
+     
+
+
     
     
     
